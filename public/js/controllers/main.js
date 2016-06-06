7567e298b651ca0187cd0d8a8020cd4d717cd3f4
Dwayne Brissett <brissettdwayne@gmail.com>
2016-06-06 10:03:49 -0400

    Fixing heroku again again again

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -29,7 +29,7 @@ angular
     }
 
     $scope.getData = function() {
-      $http.jsonp("http://api.petfinder.com/pet.find?key=58471d9327fc0e1b20cf60525e2693df&count=50&format=json&location=" + $scope.location + "&callback=JSON_CALLBACK").then(function(response){
+      $http.jsonp("https://api.petfinder.com/pet.find?key=58471d9327fc0e1b20cf60525e2693df&count=50&format=json&location=" + $scope.location + "&callback=JSON_CALLBACK").then(function(response){
         console.log(response.data.petfinder.pets.pet)
         var petFinderData = response.data.petfinder.pets.pet;
         $scope.animals = petFinderData;
